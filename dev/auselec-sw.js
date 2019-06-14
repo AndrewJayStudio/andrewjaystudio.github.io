8e726b35cb661726a44963991cb60fdab0188f44
AndrewJayStudio <andrewjaystudio@gmail.com>
2019-06-14 11:23:06 -0500

    Snapshot 1vz.5

--- dev/auselec-sw.js
+++ dev/auselec-sw.js
@@ -18,43 +18,40 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-	event.respondWith(
-		caches.match(event.request)
-			.then(function (response) {
-				// Cache hit - return response
-				if (response) {
-					return response;
-				}
-
-				return fetch(event.request).then(
-					function (response) {
-						// Check if we received a valid response
-						if (!response || response.status !== 200 || response.type !== 'basic') {
-							return response;
-						}
-
-						// IMPORTANT: Clone the response. A response is a stream
-						// and because we want the browser to consume the response
-						// as well as the cache consuming the response, we need
-						// to clone it so we have two streams.
-						var responseToCache = response.clone();
-
-						caches.open(CACHE_NAME)
-							.then(function (cache) {
-								cache.put(event.request, responseToCache);
-							});
-
-						return response;
-					}
-				);
-			})
-	);
+    event.respondWith(fromNetwork(event.request, 3000).catch(function () {
+      return fromCache(event.request);
+    }));
 });
+  
+function fromNetwork(request, timeout) {
+  return new Promise(function (resolve, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          clearTimeout(timeoutId);
+          reject;
+        }
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(function (cache) {
+            cache.put(event.request, responseToCache);
+          });
+          clearTimeout(timeoutId);
+          resolve(response);
+      }, reject);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.match(request).then(function (response) {
+      return response || Promise.reject('no-match');
+    });
+  });
+}
 
 self.addEventListener('activate', function(event) {
-
   var cacheWhitelist = [CACHE_NAME];
-
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
